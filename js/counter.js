cb9508bf0bd4ba81b8955a0b34ed21d4c4817012
Alexandr Cherepanov <cherepanov3496@gmail.com>
2022-04-26 21:58:50 +0300

    начал писать js

---
+++ js/counter.js
@@ -0,0 +1,10 @@
+console.log('hello');
+
+const btnMinus = document.querySelector('[data-action="minus"]');
+
+const btnPlus = document.querySelector('[data-action="plus"]');
+
+console.log(btnMinus);
+console.log(btnPlus);
+
+//закончил видос на 12:01
