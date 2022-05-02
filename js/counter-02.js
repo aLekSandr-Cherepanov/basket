104967e726494a6447472510834a7992f70db635
Alexandr Cherepanov <cherepanov3496@gmail.com>
2022-05-02 14:51:32 +0300

    закончил реализацию кнопок минус плюс

---
+++ js/counter-02.js
@@ -0,0 +1,33 @@
+ window.addEventListener('click', function (event) {
+
+    //Обьявили переменную для счетчика
+    let counter;
+
+    //Проверяем клик строго по кнопкам плюс либо минус
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+        //Находим обертку счетчика
+        const counterWrapper = event.target.closest('.counter-wrapper');
+
+        //Находим див с числом счетчика
+        counter = counterWrapper.querySelector('[data-counter]');
+    }
+
+    
+     
+    // Явлеяется элемент по которому был совершен клик кнопкой плюс
+     if (event.target.dataset.action === 'plus') {
+
+        counter.innerText = ++counter.innerText;
+
+     }
+
+    // Явлеяется элемент по которому был совершен клик кнопкой минус
+     if (event.target.dataset.action === 'minus') {
+
+        if ( parseInt(counter.innerText) > 0 ) {
+            // Изменяем текст в счетчике уменьшая его на 1
+            counter.innerText = --counter.innerText;  
+        }
+    }
+
+ });
