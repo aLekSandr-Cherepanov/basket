3a58dec538ec4e227eecb8f7af62ae01112ca379
Alexandr Cherepanov <cherepanov3496@gmail.com>
2022-05-10 21:54:30 +0300

    сбор данных с карточек

--- js/cart-01.js
+++ js/cart-01.js
@@ -6,8 +6,21 @@ window.addEventListener('click', function (event) {
         
         // Находим карточку с товаром, внутри которой был совершен клик
         const card = event.target.closest('.card');
-        console.log(card);
+        
+
+        //Собираем данные  с этого товара и записываем их в единый объект productInfo
+        const productInfo = {
+            id: card.dataset.id,
+            imgSrc: card.querySelector('.product-img').getAttribute('src'),
+            title: card.querySelector('.item-title').innerText,
+            itemsInBox: card.querySelector('[data-items-in-box]').innerText,
+            weight: card.querySelector('.price__weight').innerText,
+            price: card.querySelector('.price__currency').innerText,
+            counter: card.querySelector('[data-counter]').innerText,
+        };
+
+        console.log(productInfo);
     }
 })
 
-//Остановился на 54:27 мин
+//Остановился на 59:55
